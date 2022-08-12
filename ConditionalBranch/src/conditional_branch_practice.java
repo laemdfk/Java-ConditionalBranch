5bb987e3eaaeec7b7b6b42ef51e383865eddf9da
laemdfk <kappa021618@icloud.com>
2022-08-12 19:32:05 +0900

    条件分岐の基礎問題ファイル追加

---
+++ ConditionalBranch/src/conditional_branch_practice.java
@@ -0,0 +1,48 @@
+//prob3-1.(難易度：★)
+//1から10までの乱数を発生させてそれを画面に表示し、5以上ならば、”5以上です”と表示するプログラムを作りなさい。
+
+import java.util.Random;
+
+public class conditional_branch_practice {
+
+	public static void main (String arms[]) {
+
+//		ランダム表示させるメソッド→Random クラスの nextInt()を用いる
+//		nextInt() は 引数を指定した場合、0から指定した値マイナス1までの値が返される。
+//		ex:()の中が5
+//		0~4までのいずれかの変数が返される
+		
+//　　　 指定しない場合はint型が取り得る値(ex:432123458)が返される。
+
+		
+		Random rand = new Random();              //新しくRamdom表示させると定義
+		 int i = rand.nextInt(10) + 1 ;          //変数部分の定義。ここの"+"の後ろの整数を変更することで、表示する整数も変更される! 
+		 System.out.println(i);                  //いつもの。整数の表示。
+		 
+		 if (i > 5) {                            //if文「もし変数iが5より大きければ」を定義
+			 System.out.println("iは5以上です");   //「iは5以上です」を出力
+		 }
+		 
+		 
+//		 発展として、else文も追加すると、iは5以下でしたなども表示できそう。
+		 
+
+//		 下記コードのように、最初にmin/maxを定義し、書くことも可能(参考:DelftStack "Java で 1 から 10 までの乱数を生成する方法")
+     int min = 1;
+     int max = 10;
+
+     Random random = new Random();
+
+	 int value = random.nextInt(max + min) + min;
+	  System.out.println(value);
+		 
+		 
+		 
+
+//		prob3-4.(難易度：★)
+//		1から100までの乱数を発生させてそれを画面に表示し、値が10以下か、90以上なら、”10以下か90以上の値です”と表示するプログラムを作りなさい。
+//
+//		prob3-5.(難易度：★)
+//		1から100までの乱数を発生させてそれを画面に表示し、値が20以上80未満であれば、”20以上80未満です”と表示するプログラムを作りなさい。
+	}
+}
---
+++ ConditionalBranch/src/conditional_branch_practice02.java
@@ -0,0 +1,22 @@
+//prob3-2.(難易度：★)
+//1から10までの乱数を発生させてそれを画面に表示し、その値が1でなければ”１ではありません”と表示するプログラムを作りなさい。
+
+
+import java.util.Random;
+
+public class conditional_branch_practice02 {
+ public static void main(String arms[])  {
+
+	Random rand = new Random();  
+       int n = rand.nextInt(10) ;
+       System.out.println(n);
+       
+       if (n > 1) {
+     	  System.out.println("nは1ではありません");
+       }
+ }
+}
+
+
+//一つのプログラムに、同じローカル変数は使えない→→重複という形でエラーが起きた。
+//変数の命名にも注意が必要と考えられる。(一目で意味が分かりやすく、被らない)
